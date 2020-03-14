e866e0da4621f1c62570d43cd9bd28ca12099f21
Vladislav Tereshkin <firstaake@gmail.com>
2020-03-14 17:23:11 +0300

    all bugs fixed

--- src/app/components/tv-show/tv-show.component.ts
+++ src/app/components/tv-show/tv-show.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { TvShow } from '../../consts/tv-show';
 
 @Component({
@@ -9,10 +11,16 @@ import { TvShow } from '../../consts/tv-show';
 })
 export class TvShowComponent implements OnInit {
   show: TvShow = null;
+  videoURL = null;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.show = this.route.snapshot.data.show;
+    this.route.paramMap
+    .subscribe(() => {
+      this.show = this.route.snapshot.data.show;
+      this.videoURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.show.link);
+    });
   }
+
 }
